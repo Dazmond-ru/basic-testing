2178bcce41c913bf5585166fec31babb4c5ce5a1
Sergey <fserega2014@yandex.ru>
2023-07-02 17:43:28 +0300

    feat: add 02-table-tests solution

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,15 +1,14 @@
-// Uncomment the code below and write your tests
 import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-     const result = simpleCalculator({ a: 6, b: 3, action: Action.Add });
-     expect(result).toBe(9);
+    const result = simpleCalculator({ a: 6, b: 3, action: Action.Add });
+    expect(result).toBe(9);
   });
 
   test('should subtract two numbers', () => {
-     const result = simpleCalculator({ a: 6, b: 3, action: Action.Subtract });
-     expect(result).toBe(3);
+    const result = simpleCalculator({ a: 6, b: 3, action: Action.Subtract });
+    expect(result).toBe(3);
   });
 
   test('should multiply two numbers', () => {
@@ -23,7 +22,11 @@ describe('simpleCalculator tests', () => {
   });
 
   test('should exponentiate two numbers', () => {
-    const result = simpleCalculator({ a: 6, b: 3, action: Action.Exponentiate });
+    const result = simpleCalculator({
+      a: 6,
+      b: 3,
+      action: Action.Exponentiate,
+    });
     expect(result).toBe(216);
   });
 
